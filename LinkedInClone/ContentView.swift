bea1265e06b3eb8bfdcb70f052e49f2e61634624
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-09-23 16:27:38 -0400

    Initial Commit

---
+++ LinkedInClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LinkedInClone
+//
+//  Created by Sai Lakshmi on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LinkedInClone/LinkedInCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInCloneApp.swift
+//  LinkedInClone
+//
+//  Created by Sai Lakshmi on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
